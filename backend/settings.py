bee01171a9f6868b9d3942731a8222af6f53508a
Délita Makanda <delitamakanda@users.noreply.github.com>
2020-04-13 20:02:49 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
