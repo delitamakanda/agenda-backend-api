c2b51631fc10246d0af32b4951afe66dd5845a5b
Délita Makanda <delitamakanda@users.noreply.github.com>
2020-04-18 19:55:27 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -147,7 +147,7 @@ import datetime
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
